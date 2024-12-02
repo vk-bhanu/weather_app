cd2d77b57f2ac30ed73bc09c59d7dad42a367440
vk-bhanu <146329087+vk-bhanu@users.noreply.github.com>
2024-12-02 14:49:36 +0530

    ReactJS WeatherApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Search from './components/search/Search'
+import CurrentWeather from './components/current-weather/CurrentWeather'
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api'
+import Forecaste from './components/forecast/Forecaste'
 
-function App() {
+const App = () => {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForcast] = useState(null);
+  const handleOnSearchChange = (searchData)=>{
+    const [lat, long] = searchData.value.split(" ");
+    const CurrentFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}&units=metric`);
+    const ForecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([CurrentFetch, ForecastFetch])
+      .then(async (response) =>{
+        const weatherRes = await response[0].json();
+        const forecastRes = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherRes });
+        setForcast({ city: searchData.label, ...forecastRes});
+      })
+      .catch((err) =>{
+        console.log(err);
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecaste data={forecast} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c5a22779f5mshd20ae71db0e9834p1ce22ejsn51e82fad47c0",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "54ec20be4b1026cbdc9fe4c4848d0492";
